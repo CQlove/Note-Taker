d1f16715bf081d6f6370a4ac29770651200c0115
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-14 19:15:44 -0600

    add go back button in notes.html

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const db = require('./db');
+const path = require('path');
+
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+
+// added path
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
