9a08dd2ce91b5bf724375b400ea6d09ba412bcb2
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-15 16:56:44 -0600

    added post routes or api routes in server.js

--- server.js
+++ server.js
@@ -16,11 +16,44 @@ app.get('/api/notes', (req, res) => {
     fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return res.status(400).json({ error: 'Failed to read notes, please check the routes' });
+            return res.status(400).json({ error: 'Failed to read notes, please check the routes or there is no saved note' });
         }
 
         const notes = JSON.parse(data);
         res.json(notes);
+    });
+});
+
+// added post routes to read and write bd.json
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    // setting obj to save
+    const inputNote = {
+        id: uuidv4(),
+        title: title,
+        text: text,
+        // toLocaleTime transfer a readable time to me
+        date: new Date().toLocaleTimeString(),
+    };
+    // read db.json
+    fs.readFile(path.join(__dirname, './Develop/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(400).json({ error: 'Failed to read notes' });
+        }
+
+        const notes = JSON.parse(data);
+
+        notes.push(inputNote);
+        // write into db.json
+        fs.writeFile(path.join(__dirname, './Develop/db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to save note' });
+            }
+
+            res.json(inputNote);
+        });
     });
 });
 
