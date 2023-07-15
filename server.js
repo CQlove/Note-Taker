c63a6c04d2f55b057b9be85b0dfe83fff4c78516
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-15 16:31:59 -0600

    finished setting

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+const fs = require('fs');
 const express = require('express');
-const db = require('./db');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-
+// setting application of express; port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./Develop/public'));
 
 
 // added path
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Develop', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
