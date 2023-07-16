d27a085871a43ed07d6e53f96296da5acb8ec730
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-07-16 16:08:12 -0600

    add delete method and use array.fliter() get new array

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -47,4 +47,29 @@ api.post('/api/notes', (req, res) => {
     });
 });
 
+api.delete('/api/notes/:id', (req, res) => {
+    const deleteId = req.params.id;
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(400).json({ error: 'Failed to read notes' });
+        }
+
+        const notes = JSON.parse(data);
+        // use array.filter() to find matched id, array here is notes
+        const newNotes = notes.filter((db) => db.id !== deleteId);
+        // same length means notes and newNotes are no difference
+        if (notes.length === newNotes.length) {
+            return res.status(404).json({ oops: 'Can not find id in database! Please check id carefully!' })
+        }
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(newNotes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to delete note' });
+            }
+            res.status(200).json({ wow: 'You just delete a note.' })
+        });
+    });
+});
 module.exports = api;
